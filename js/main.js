c7014e2cda7eca381f2f2b03e920b0785d3ae49f
hydrae-soft <torres.lf21@gmai.com>
2023-05-20 14:55:58 -0300

    Finished: mobile view

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
-const columns = document.querySelectorAll(".column")
+const columns = document.querySelectorAll('.column')
+const column_content = document.querySelectorAll('.column .content')
 let activeColumn = 0
-columns.forEach((column, index) =>
-{
-    column.addEventListener("click", function()
-    {
-        switchColumn(index)
+columns.forEach((column, index) => {
+    column.addEventListener('click', () => {
+        columns[activeColumn].classList.remove('active')
+        column_content[activeColumn].classList.remove('active')
+        columns[index].classList.add('active')
+        column_content[index].classList.add('active')
+        activeColumn = index
     })
 })
-
-function switchColumn(index)
-{
-    columns[activeColumn].classList.remove("active")
-    columns[index].classList.add("active")
-    activeColumn = index
-}
